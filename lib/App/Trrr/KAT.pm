b1b46d8306a4b17895971ea6db7b00528c89a723
z448 <zed448@icloud.com>
2018-04-01 09:20:57 +0200

    regex bug

--- lib/App/Trrr/KAT.pm
+++ lib/App/Trrr/KAT.pm
@@ -30,14 +30,31 @@ sub kat {
     my( @item, %t ) = ();
     open(my $fh,"<",\$response->{content}) || die "cant open response $!";
     while(<$fh>){
-        s/(.*\{ 'name': ')(.*?)(\'.*)(magnet\:.*?)('.*)/$2$4/ and $t{title} = $2 and $t{magnet} = $4 if /data-sc-params="\{ 'name'\:/;
-        $t{title} =~ s/%([0-9A-Fa-f]{2})/chr(hex($1))/eg;
-        $t{size} = $_ and $t{size} =~ s/(<td class="nobr center">)(.*?)( <span>)(.)(.*)/$2$4/ if /<td class="nobr center">/;
-        $t{category} = $_ and $t{category} =~ s/(.*?span id="cat_.*?href=".*?">)(.*?)(<\/a.*)/$2/ if /^Posted/;
-        $t{seeds} = $_ and $t{seeds} =~ s/(<td class="green center">)(.*?)(\<.*)/$2/ if /<td class="green center">/;
+        if(/data-sc-params="\{ 'name'\:/){
+            s/(.*\{ 'name': ')(.*?)(\'.*)(magnet\:.*?)('.*)/$2$4/;
+            $t{title} = $2;
+            $t{magnet} = $4;
+            $t{title} =~ s/%([0-9A-Fa-f]{2})/chr(hex($1))/eg;
+        };
+
+        if(/<td class="nobr center">/){
+            $t{size} = $_;
+            $t{size} =~ s/(<td class="nobr center">)(.*?)( <span>)(.)(.*)/$2$4/;
+        }
+
+        if(/^Posted/){
+            $t{category} = $_;
+            $t{category} =~ s/(.*?span id="cat_.*?href=".*?">)(.*?)(<\/a.*)/$2/;
+        }
+        
+        if(/<td class="green center">/){
+            $t{seeds} = $_;
+            $t{seeds} =~ s/(<td class="green center">)(.*?)(\<.*)/$2/;
+        }
 
         if(/<td class="red lasttd center">/){
-            $t{leechs} = $_; $t{leechs} =~ s/(<td class="red lasttd center">)(.*?)(\<.*)/$2/;
+            $t{leechs} = $_;
+            $t{leechs} =~ s/(<td class="red lasttd center">)(.*?)(\<.*)/$2/;
             chomp($t{magnet}, $t{title}, $t{size}, $t{category}, $t{seeds}, $t{leechs});
             push @item, {%t};
         }
