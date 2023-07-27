a5749d76cbe69c0379332f30f27c0cefa76af355
z448 <zed448@icloud.com>
2023-07-27 11:42:06 +0200

    TPB.pm: category numbers to words hash

--- lib/App/Trrr/TPB.pm
+++ lib/App/Trrr/TPB.pm
@@ -26,14 +26,63 @@ sub tpb {
     my $json = JSON::PP->new->ascii->pretty->allow_nonref;
     my $content = $json->decode($response->{content});
 
-    my( @item, %t, $leechs ) = ();
+    my( @item, %t ) = ();
+    my %category = (
+	    101 => "Audio > Music",
+	    102 => "Audio > Books",
+	    103 => "Audio > Sound Clips",
+	    104 => "Audio > FLAC",
+	    199 => "Audio > Other",
+	    201 => "Video > Movies",
+	    202 => "Video > Movies DRDR",
+	    203 => "Video > Music Videos",
+	    204 => "Video > Movie Clips",
+	    205	=> "Video > TV-Shows",
+	    206 => "Video > Handheld",
+	    207 => "Video > HD Movies",
+	    208	=> "Video > HD TV-Shows",
+	    209 => "Video > 3D",
+	    299 => "Video > Other",
+	    301 => "Applications > Windows",
+	    302 => "Applications > Mac/Apple",
+	    303 => "Applications > Unix",
+	    304 => "Applications > Handheld",
+	    305 => "Applications > IOS(iPad/iPhone)",
+	    306 => "Applications > Andriod",
+	    399 => "Applications > Other OS",
+	    401 => "Games > PC",
+	    402 => "Games > Mac/Apple",
+	    403 => "Games > PSx",
+	    404 => "Games > XBOX360",
+	    405 => "Games > Wii",
+	    406 => "Games > Handheld",
+	    407 => "Games > IOS(iPad/iPhone)",
+	    408 => "Games > Android",
+	    499 => "Games > Other OS",
+	    501 => "Porn > Movies",
+	    502 => "Porn > Movies DVDR",
+	    503 => "Porn > Pictures",
+	    504 => "Porn > Games",
+	    505 => "Porn > HD-Movies",
+	    506 => "Porn > Movie Clips",
+	    599 => "Porn > Other",
+	    601 => "Other > E-books",
+	    602 => "Other > Comics",
+	    603 => "Other > Pictures",
+	    604 => "Other > Covers",
+	    605 => "Other > Physibles",
+	    606 => "Other > Other"
+    );
+
+    
     for(@$content){
         $t{title} = $_->{name};
 	$t{magnet} = "magnet:?xt=urn:btih:" . $_->{info_hash} . "&dn=" . $t{title};
 	$t{size} = $_->{size};
 	$t{seeds} = $_->{seeders};
 	$t{leechs} = $_->{leechers};
-	$t{category} = $_->{category};
+	$t{category} = $category{"$_->{category}"};
+	
 	push @item, {%t};
     }
     return \@item;
