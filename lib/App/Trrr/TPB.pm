44311a079bfade1cf3a1fdfa70fae4b5a354f68c
z448 <zed448@icloud.com>
2023-08-18 09:35:21 +0200

    TPB.pm: rewrite

--- lib/App/Trrr/TPB.pm
+++ lib/App/Trrr/TPB.pm
@@ -10,12 +10,15 @@ App::Trrr::TPB - TPB API
 @EXPORT_OK = qw( tpb );
 our $VERSION = '0.01';
 
+use v5.10;
 use strict;
 use warnings;
+use JSON;
 use Carp;
 use HTTP::Tiny;
-use JSON::PP;
+use JSON;
 use List::Util qw(first);
+use Data::Dumper;
 
 sub size {
     my $bytes = shift;
@@ -51,6 +54,129 @@ sub size {
     }
 }
 
+
+my %category = (
+    101 => "Audio > Music",
+    102 => "Audio > Books",
+    103 => "Audio > Sound Clips",
+    104 => "Audio > FLAC",
+    199 => "Audio > Other",
+    201 => "Video > Movies",
+    202 => "Video > Movies DRDR",
+    203 => "Video > Music Videos",
+    204 => "Video > Movie Clips",
+    205	=> "Video > TV-Shows",
+    206 => "Video > Handheld",
+    207 => "Video > HD Movies",
+    208	=> "Video > HD TV-Shows",
+    209 => "Video > 3D",
+    211 => "Video > UHD/4k Movies",
+    299 => "Video > Other",
+    301 => "Applications > Windows",
+    302 => "Applications > Mac/Apple",
+    303 => "Applications > Unix",
+    304 => "Applications > Handheld",
+    305 => "Applications > IOS(iPad/iPhone)",
+    306 => "Applications > Andriod",
+    399 => "Applications > Other OS",
+    401 => "Games > PC",
+    402 => "Games > Mac/Apple",
+    403 => "Games > PSx",
+    404 => "Games > XBOX360",
+    405 => "Games > Wii",
+    406 => "Games > Handheld",
+    407 => "Games > IOS(iPad/iPhone)",
+    408 => "Games > Android",
+    499 => "Games > Other OS",
+    501 => "Porn > Movies",
+    502 => "Porn > Movies DVDR",
+    503 => "Porn > Pictures",
+    504 => "Porn > Games",
+    505 => "Porn > HD-Movies",
+    506 => "Porn > Movie Clips",
+    599 => "Porn > Other",
+    601 => "Other > E-books",
+    602 => "Other > Comics",
+    603 => "Other > Pictures",
+    604 => "Other > Covers",
+    605 => "Other > Physibles",
+    699 => "Other > Other"
+);
+
+
+sub results {
+    my( $content, $domain ) = @_;
+
+    my( @item, %t ) = ();
+    if( $domain eq 'apibay.org' ){
+	$content = decode_json($content);
+
+    	open(my $fh,"<",\$content) || die "cant open \$content: $!";
+    	while(<$fh>){
+            for(@$content){
+                $t{title} = $_->{name};
+	    
+                $t{magnet} = "magnet:?xt=urn:btih:" . $_->{info_hash} . "&dn=" . $t{title};
+        	$t{magnet} =~ s/ /%20/g;
+        	$t{magnet} =~ s/"/%22/g;
+	    
+        	$t{size} = size($_->{size});
+	        $t{seeds} = $_->{seeders};
+	        $t{leechs} = $_->{leechers};
+	        $t{category} = $category{"$_->{category}"};
+	
+	    	push @item, {%t};
+    	    }
+    	    return \@item;
+	}
+    	close $fh;
+    } else {
+	my %in = ( table => 0, seeds => 1 );
+    	open(my $fh,"<",\$content) || die "cant open \$content: $!";
+    	while(<$fh>){
+	    $in{table} = 1 if /\(<a href="/;    
+	    #$in{table} = 1 if /^<tr>\r/;    
+	    #$in{table} = 1 if /^<div class="detName">/;    
+	    $in{table} = 0 if /\t<\/tr>\r$/;    
+
+	    if(/\(<a href=".+?>(.+?)</ and $in{table}){
+	        $t{category} = $1;
+		$t{category} =~ s/ - / > /;
+	    }
+
+	    if(/^<div class="detName">.+>(.+?)<\/a>\r/ and $in{table}){
+	        $t{title} = $1;
+	    }
+
+	    if(/^<a href="(magnet:.+?)".+<a href=".+\/user\/(.+?)\/.+Uploaded (.+?), Size (.+?),/ and $in{table}){
+	        $t{magnet} = $1;
+	        $t{user} = $2; 
+		$t{added} = $3;
+		$t{size} = $4; 
+		$t{added} =~ s/&nbsp;/-/;
+		$t{size} =~ s/&nbsp;/\ /; $t{size} =~ s/iB/b/;
+	    }
+	    
+	    if(/<td align="right">(\d+?)<\/td>\r/ and $in{table}){
+  	        if( $in{seeds} % 2 ){
+		    $t{seeds} = $1;
+		    $in{seeds}++; 
+	        } else {
+	              $t{leechs} = $1;
+		      $in{seeds}++;  
+		  }
+	    }
+
+	    if(/\t<\/tr>\r/){
+		push @item, {%t};
+	    }
+	}
+	close $fh;
+	return \@item;
+    }
+}
+
+
 sub tpb {
     my $keywords = shift;
     my @domain = (
@@ -72,140 +198,16 @@ sub tpb {
     for( @domain ){
 	    if(/^apibay\.org$/){
 		    $url = 'https://' . $_ . '/q.php?q=' . join('%20', @$keywords) . '&cat=0';
-		    $response = HTTP::Tiny->new->get($url);
-		    next unless $response->{success};
-		    return apibay($response->{content}) if $response->{success};
 	    } else {
 		    $url = 'https://' . $_ . '/search/' . join('%20', @$keywords) . '/1/99/0';
-		    $response = HTTP::Tiny->new->get($url) ;
-		    if( !($response->{success}) and ($_ eq $domain[$#domain]) ){ die "non of the domains works:\n" . join("\n", @domain) }
-		    
-		    next unless $response->{success};
-
-		    return mirror($response->{content}) if $response->{success};
     	    }
+	    
+	    $response = HTTP::Tiny->new->get($url) ;
+ 	    if( !($response->{success}) and ($_ eq $domain[$#domain]) ){ die "non of the domains works:\n" . join("\n", @domain) }
+	    next unless $response->{success};
+	    return results($response->{content}, $_) if $response->{success};
     }
-
-
 } 
-
-
-sub apibay {
-    my $content = shift;
-     
-    my $json = JSON::PP->new->ascii->pretty->allow_nonref;
-    $content = $json->decode($content);
-
-    my( @item, %t ) = ();
-    my %category = (
-	    101 => "Audio > Music",
-	    102 => "Audio > Books",
-	    103 => "Audio > Sound Clips",
-	    104 => "Audio > FLAC",
-	    199 => "Audio > Other",
-	    201 => "Video > Movies",
-	    202 => "Video > Movies DRDR",
-	    203 => "Video > Music Videos",
-	    204 => "Video > Movie Clips",
-	    205	=> "Video > TV-Shows",
-	    206 => "Video > Handheld",
-	    207 => "Video > HD Movies",
-	    208	=> "Video > HD TV-Shows",
-	    209 => "Video > 3D",
-	    211 => "Video > UHD/4k Movies",
-	    299 => "Video > Other",
-	    301 => "Applications > Windows",
-	    302 => "Applications > Mac/Apple",
-	    303 => "Applications > Unix",
-	    304 => "Applications > Handheld",
-	    305 => "Applications > IOS(iPad/iPhone)",
-	    306 => "Applications > Andriod",
-	    399 => "Applications > Other OS",
-	    401 => "Games > PC",
-	    402 => "Games > Mac/Apple",
-	    403 => "Games > PSx",
-	    404 => "Games > XBOX360",
-	    405 => "Games > Wii",
-	    406 => "Games > Handheld",
-	    407 => "Games > IOS(iPad/iPhone)",
-	    408 => "Games > Android",
-	    499 => "Games > Other OS",
-	    501 => "Porn > Movies",
-	    502 => "Porn > Movies DVDR",
-	    503 => "Porn > Pictures",
-	    504 => "Porn > Games",
-	    505 => "Porn > HD-Movies",
-	    506 => "Porn > Movie Clips",
-	    599 => "Porn > Other",
-	    601 => "Other > E-books",
-	    602 => "Other > Comics",
-	    603 => "Other > Pictures",
-	    604 => "Other > Covers",
-	    605 => "Other > Physibles",
-	    699 => "Other > Other"
-    );
-
-    
-    for(@$content){
-        $t{title} = $_->{name};
-	    
-        $t{magnet} = "magnet:?xt=urn:btih:" . $_->{info_hash} . "&dn=" . $t{title};
-        $t{magnet} =~ s/ /%20/g;
-        $t{magnet} =~ s/"/%22/g;
-	    
-        $t{size} = size($_->{size});
-	    $t{seeds} = $_->{seeders};
-	    $t{leechs} = $_->{leechers};
-	    $t{category} = $category{"$_->{category}"};
-	
-	    push @item, {%t};
-    }
-    return \@item;
-}
-
-
-sub mirror {
-    my $content = shift;
-
-    my $category = 0;
-    my( @item, %t, $leechs, ) = ();
-    open(my $fh,"<",\$content) || die "cant open \$content: $!";
-    while(<$fh>){
-            $t{title} = $_ and $t{title} =~ s/(.*?title\=\"Details for )(.*?)(\".*)/$2/ if /detName/;
-            
-            $t{magnet} = $_ and $t{magnet} =~ s/(\<a href\=\")(magnet.*?)(\".*)/$2/  if /\<a href\=\"magnet/;
-            $t{magnet} =~ s/ /%20/g;
-            $t{magnet} =~ s/"/%22/g;
-            
-            $t{size} = $_ and $t{size} =~ s/(.*?)(Size.*?\ )(.*?)(\&nbsp\;)(.)i(.)(.*)/$3$5b/ if /Size.*?\ /;
-
-	if(/<td align="right">/){  
-	    $t{api} = 'tpb';	
-	    unless($leechs){
-	        $t{seeds} = $_; $t{seeds} =~ s/(.*?<td align="right">)([0-9]+)(<.*)/$2/; $leechs = 1;
-	    } else { $t{leechs} = $_; $t{leechs} =~ s/(.*?<td align="right">)([0-9]+)(<.*)/$2/; $leechs = 0 }
-	}
-        if(/More from this category/){
-            if($category == 0){
-                $t{cate} = $_ and $t{cate} =~ s/(.*category\"\>)(.*?)(\<.*)/$2/;
-		#$t{category} = $_ and $t{category} =~ s/(.*category\"\>)(.*?)(\<.*)/$2/;
-                chomp($t{magnet}, $t{title}, $t{size}, $t{cate}, $t{seeds}, $t{leechs});
-		#chomp($t{magnet}, $t{title}, $t{size}, $t{category}, $t{seeds}, $t{leechs});
-                push @item, {%t};
-                $category = 1;
-	    #} else { $category = 0 }
-            } else {  # <-----from here
-                    $t{gory} = $_ and $t{gory} =~ s/(.*category\"\>)(.*?)(\<.*)/$2/;
-		    chomp($t{gory});
-		    #$t{category} = $_ and $t{category} =~ s/(.*category\"\>)(.*?)(\<.*)/$2/;
-		    $t{category} = $t{cate} . ' > ' . $t{gory}; 
-		    $category = 0;
-	    } # <----- to here
-        }
-    }
-    close $fh;
-    return \@item;
-}
 
 
 1;
