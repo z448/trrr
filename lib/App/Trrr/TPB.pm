009cbb6693bd20e948507c409e96270c24e358db
z448 <zed448@icloud.com>
2023-08-20 16:56:21 +0200

    trrr: rewrite; TPB.pm: categories shortened

--- lib/App/Trrr/TPB.pm
+++ lib/App/Trrr/TPB.pm
@@ -52,51 +52,51 @@ sub size {
 
 
 my %category = (
-    101 => "Audio > Music",
-    102 => "Audio > Books",
-    103 => "Audio > Sound Clips",
-    104 => "Audio > FLAC",
-    199 => "Audio > Other",
-    201 => "Video > Movies",
-    202 => "Video > Movies DRDR",
-    203 => "Video > Music Videos",
-    204 => "Video > Movie Clips",
-    205	=> "Video > TV-Shows",
-    206 => "Video > Handheld",
-    207 => "Video > HD Movies",
-    208	=> "Video > HD TV-Shows",
-    209 => "Video > 3D",
-    211 => "Video > UHD/4k Movies",
-    299 => "Video > Other",
-    301 => "Applications > Windows",
-    302 => "Applications > Mac/Apple",
-    303 => "Applications > Unix",
-    304 => "Applications > Handheld",
-    305 => "Applications > IOS(iPad/iPhone)",
-    306 => "Applications > Andriod",
-    399 => "Applications > Other OS",
-    401 => "Games > PC",
-    402 => "Games > Mac/Apple",
-    403 => "Games > PSx",
-    404 => "Games > XBOX360",
-    405 => "Games > Wii",
-    406 => "Games > Handheld",
-    407 => "Games > IOS(iPad/iPhone)",
-    408 => "Games > Android",
-    499 => "Games > Other OS",
-    501 => "Porn > Movies",
-    502 => "Porn > Movies DVDR",
-    503 => "Porn > Pictures",
-    504 => "Porn > Games",
-    505 => "Porn > HD-Movies",
-    506 => "Porn > Movie Clips",
-    599 => "Porn > Other",
-    601 => "Other > E-books",
-    602 => "Other > Comics",
-    603 => "Other > Pictures",
-    604 => "Other > Covers",
-    605 => "Other > Physibles",
-    699 => "Other > Other"
+    101 => "Music",
+    102 => "Audio Books",
+    103 => "Sound Clips",
+    104 => "Audio FLAC",
+    199 => "Audio Other",
+    201 => "Movies",
+    202 => "Movies",
+    203 => "Music Videos",
+    204 => "Movie Clips",
+    205	=> "TV-Shows",
+    206 => "Handheld",
+    207 => "HD Movies",
+    208	=> "HD TV-Shows",
+    209 => "3D",
+    211 => "UHD/4k Movies",
+    299 => "Other Videos",
+    301 => "Windows",
+    302 => "Mac",
+    303 => "Unix",
+    304 => "Handheld",
+    305 => "iOS",
+    306 => "Andriod",
+    399 => "Other OS",
+    401 => "PC",
+    402 => "Mac",
+    403 => "PSX",
+    404 => "XBOX360",
+    405 => "Wii",
+    406 => "Handheld",
+    407 => "iOS",
+    408 => "Android",
+    499 => "Games",
+    501 => "Porn Movies",
+    502 => "Porn Movies",
+    503 => "Porn Pictures",
+    504 => "Porn Games",
+    505 => "Porn HD",
+    506 => "Porn Clips",
+    599 => "Porn",
+    601 => "E-books",
+    602 => "Comics",
+    603 => "Pictures",
+    604 => "Covers",
+    605 => "Physibles",
+    699 => "Other"
 );
 
 
@@ -111,6 +111,7 @@ sub results {
     	while(<$fh>){
             for(@$content){
                 $t{title} = $_->{name};
+		$t{api} = 'tpb';
 	    
                 $t{magnet} = "magnet:?xt=urn:btih:" . $_->{info_hash} . "&dn=" . $t{title};
         	$t{magnet} =~ s/ /%20/g;
@@ -140,6 +141,7 @@ sub results {
 
 	    if(/^<div class="detName">.+>(.+?)<\/a>\r/ and $in{table}){
 	        $t{title} = $1;
+		$t{api} = 'tpb';
 	    }
 
 	    if(/^<a href="(magnet:.+?)".+Uploaded (.+?), Size (.+?),(.+)(\/|>)(.+?)<\/(i>|a> )<\/font>\r/ and $in{table}){ 
