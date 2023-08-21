c7a70880e791d2f850388b211c40186e1f718ad4
z448 <zed448@icloud.com>
2023-08-21 10:12:12 +0200

    Trrr.pm: rewritten; trrr: using open_magnet function now; lots of errors 'Use of uninitialized value in string eq at /home/z/perl5/bin/trrr line 123.'

--- lib/App/Trrr.pm
+++ lib/App/Trrr.pm
@@ -7,107 +7,49 @@ App::Trrr - search torrents
 =cut
 
 @ISA = qw(Exporter);
-@EXPORT_OK = qw( open_app );
-our $VERSION = '0.14';
+@EXPORT_OK = qw( open_magnet );
+our $VERSION = '0.15';
 
 use strict;
+use warnings;
 
-# check if xdg-open utility is installed to use on linux
-my $check_xdg = sub {
-    my $xdg = shift;
-    $xdg = `which $xdg` and chomp $xdg;
-    if( $xdg =~ /\/xdg-open/ ){
-        return $xdg
-        } else { return "echo 'xdg-open command not found, cant open '" }
-};
 
-sub open_app {
-    my $url = shift;
-    #$url =~ s/\ /\\ /g;
-    my $os = {  osx     => 'open',
-                ios     => 'echo',
-                ubuntu  => 'xdg-open',
-                linux   => 'xdg-open',
-    };
-    if($^O eq 'MSWin32' or $^O eq 'msys'){ system("$url"); return }
-    open my $pipe,"-|",'uname -a';
-    while(<$pipe>){
-        if(/iPhone/){ system("$os->{ios} '$url   ' | pbcopy") }
-        elsif(/Darwin/){ system("$os->{osx} '$url'") }
-        elsif(/Ubuntu/){ system("$os->{ubuntu} '$url'") }
-	elsif(/Linux/){ my $open = $check_xdg->($os->{linux}); system("$open '$url'") }
+sub dep{
+    my $dep = shift;
+
+    open( my $depph, "-|", 'which', $dep );
+    while(<$depph>){
+        chomp;
+        return $_ if /\/$dep$/;
     }
-};
+}
 
+
+sub os{
+    my $os = $^O;
+
+    open(my $os_ph, '-|', 'uname', '-a');
+    while(<$os_ph>){
+        $os = 'iOS' if /iPhone/;
+    }
+    close $os_ph;
+
+    return $os;
+}
+
+
+sub open_magnet{
+    my $magnet = shift;
+
+    if(os() eq 'MSWin32' or os() eq 'msys'){
+        system("$magnet") and exit;
+    } elsif( os() =~ /iOS/){
+        print "Can't read pasteboard content, 'pbcopy' tool not installed.\n" and exit unless dep('pbcopy');
+        system("echo '$magnet   ' | pbcopy") and print "Magnet link has been placed into pasteboard\n";
+    } elsif( os() =~ /(linux|darwin)/ ){
+        print "Can't open magnet link, 'open' tool not installed.\n" and exit unless dep('open');
+        system("open '$magnet'");  
+    }
+}
 
 1;
-
-=head1 DESCRIPTION
-    
-CLI tool to search torrents. Results are sorted by number of seeders and each is mapped to key. Pressing the key with assigned letter will open magnet link in your default torrent client. On iOS, magnet link is placed into clipboard.
-
-=head1 USAGE
-    
-Search with as many parameters as needed. 
-
-=over 10
-
-C<trrr keyword1 keyword2 keywordN>
-
-=back
-
-_
-
-On Linux, start it without any parameter and it'll use clipboard content as keywords. ( needs 'xclip' or 'xsel' to be installed )
-
-=over 10
-
-C<trrr>
-
-=back
-
-_
-
-Limit results which have at least 100 seeders.
-
-=over 10
-
-C<trrr keyword1 keyword2 keywordN -100>
-
-=back
-
-_
-
-To open any magnet link from results automaticaly add C<-[key]>. This is mandatory on Windows running 'Git/Bash for Windows' where you have to specify key on CLI upfront.
-
-Open first (A) magnet link from results automaticaly.
-
-=over 10
-
-C<trrr keyword1 keyword2 keywordN -a>
-
-=back
-
-_
-
-See this perdoc.
-
-=over 10
-
-C<trrr -h>
-
-=back
-
-=head1 AUTHOR
-
-Zdenek Bohunek. <zdenek@cpan.org>
-
-App::Trr::HotKey is taken from StackOverflow post by brian d foy
-
-=head1 COPYRIGHT AND LICENSE
-
-Copyright 2016 by Zdenek Bohunek
-
-This library is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
-
-=cut
