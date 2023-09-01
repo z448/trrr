2b12fa8c8974084e09ff00b4f575ca9f8f988bc1
z448 <zed448@icloud.com>
2023-09-01 10:07:11 +0200

    Clipboard.pm: prints if no clipboard tools are installed

--- lib/App/Trrr/Clipboard.pm
+++ lib/App/Trrr/Clipboard.pm
@@ -1,102 +1,115 @@
 package App::Trrr::Clipboard;
 
-@ISA = qw(Exporter);
+@ISA       = qw(Exporter);
 @EXPORT_OK = qw( clipboard );
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
-use v5.10;
 use strict;
-use warnings;
-use autodie;
 
 my %tool = (
-    linux   =>  {
-        read    =>  [ 'xclip -o', 'xsel -o' ],
-        write   =>  [ 'xclip -i', 'xsel -i' ]
+    linux => {
+        read  => [ 'xclip -o', 'xsel -o' ],
+        write => [ 'xclip -i', 'xsel -i' ]
     },
-    ios     =>  {
-        read    =>  [ 'pbpaste' ],
-        write   =>  [ 'pbcopy']
+    ios => {
+        read  => ['pbpaste'],
+        write => ['pbcopy']
     },
-    darwin  =>  {
-        read    =>  [ 'pbpaste' ],
-        write   =>  [ 'pbcopy' ]
+    darwin => {
+        read  => ['pbpaste'],
+        write => ['pbcopy']
     },
-        msys =>  {  # could be  also 'MSWin32' ? <---
-        read    =>  [ '/dev/clipboard' ],
-        write   =>  [ '/dev/clipboard' ]
+    msys => {    # could be  also 'MSWin32' ? <---
+        read  => ['/dev/clipboard'],
+        write => ['/dev/clipboard']
     }
 );
 
-
 my $os = $^O;
-open(my $os_ph, '-|', 'uname', '-a');
-while(<$os_ph>){
+open( my $os_ph, '-|', 'uname', '-a' );
+while (<$os_ph>) {
     $os = 'ios' if /iPhone/;
 }
 close $os_ph;
 
-
-sub dep{
+sub dep {
     my $dep = shift;
     $dep =~ s/^(.+?) .+/$1/;
 
     open( my $depph, "-|", 'which', $dep );
-    while(<$depph>){
+    while (<$depph>) {
         chomp;
         return $_ if /\/$dep$/;
     }
 }
 
-
-sub clipboard{
+sub clipboard {
     my $in = shift;
 
-    if( $os eq 'ios' ){
-	my $pb_dir = '/private/var/mobile/Library/Caches/com.apple.Pasteboard';
-    	if( -e $pb_dir ){
-	    opendir(my $pb_dir_dh, $pb_dir) || die "Can't opendir $pb_dir: $!";
-	    my( $pb_dir_dir ) = grep { !/^\./ && !/^\.\.$/ && -d "$pb_dir/$_" } readdir($pb_dir_dh);
-	    closedir $pb_dir_dh;
+    if ( $os eq 'ios' ) {
+        my $pb_dir = '/private/var/mobile/Library/Caches/com.apple.Pasteboard';
+        if ( -e $pb_dir ) {
+            opendir( my $pb_dir_dh, $pb_dir )
+              || die "Can't opendir $pb_dir: $!";
+            my ($pb_dir_dir) = grep { !/^\./ && !/^\.\.$/ && -d "$pb_dir/$_" }
+              readdir($pb_dir_dh);
+            closedir $pb_dir_dh;
 
-	    opendir(my $pb_dir_dir_dh, "$pb_dir/$pb_dir_dir") || die "Can't opendir $pb_dir/$pb_dir_dir: $!";
-	    my @pb_dir_dir_file = grep { -f "$pb_dir/$pb_dir_dir/$_" } readdir($pb_dir_dir_dh);
-	    closedir $pb_dir_dir_dh;
+            opendir( my $pb_dir_dir_dh, "$pb_dir/$pb_dir_dir" )
+              || die "Can't opendir $pb_dir/$pb_dir_dir: $!";
+            my @pb_dir_dir_file =
+              grep { -f "$pb_dir/$pb_dir_dir/$_" } readdir($pb_dir_dir_dh);
+            closedir $pb_dir_dir_dh;
 
-	    for my $file( @pb_dir_dir_file ){
-		next if -s "$pb_dir/$pb_dir_dir/$file" == 38 ;
-	        open(my $ph, '-|', 'file', "$pb_dir/$pb_dir_dir/$file") || die "Can't open 'file' pipe to file $pb_dir/$pb_dir_dir/$file: $!";
-		while(<$ph>){
-		    next unless /text, with no line terminators$/;
+            for my $file (@pb_dir_dir_file) {
+                next if -s "$pb_dir/$pb_dir_dir/$file" == 38;
+                open( my $ph, '-|', 'file', "$pb_dir/$pb_dir_dir/$file" ) || die "Can't open 'file' pipe to file $pb_dir/$pb_dir_dir/$file: $!";
+                while (<$ph>) {
+                    next unless /text, with no line terminators$/;
 
-		    if($in){
-			open(my $fh, '>', "$pb_dir/$pb_dir_dir/$file") || die "Can't open file for writing $pb_dir/$pb_dir_dir/$file: $!";
-			print $fh $in;
-			close $fh;
-			return $in;
-		    } else {
-		        open(my $fh, '<', "$pb_dir/$pb_dir_dir/$file") || die "Can't open file for reading $pb_dir/$pb_dir_dir/$file: $!";
-		        while(<$fh>){
-			    return $_;
-		        }
-		    	close $fh;
-		    } 
-		}
-		close $ph;
-	    }
-	} else { $in = "$in   " if $os eq 'ios' }
+                    if ($in) {
+                        open( my $fh, '>', "$pb_dir/$pb_dir_dir/$file" ) || die "Can't open file for writing $pb_dir/$pb_dir_dir/$file: $!";
+                        print $fh $in;
+                        close $fh;
+                        return $in;
+                    }
+                    else {
+                        open( my $fh, '<', "$pb_dir/$pb_dir_dir/$file" ) || die "Can't open file for reading $pb_dir/$pb_dir_dir/$file: $!";
+                        while (<$fh>) {
+                            return $_;
+                        }
+                        close $fh;
+                    }
+                }
+                close $ph;
+            }
+        }
+        else { $in = "$in   " if $os eq 'ios' } # if it's iOS and com.apple.Pasteboard dir doesn't exist prepare $in for iOS pbcopy and continue below
     }
 
-    if($in){
-        for( @{$tool{$os}->{write}} ){
-            if( dep($_) ){ system("echo '$in' | $_") }
+    if ($in) {
+        my @tool = @{ $tool{$os}->{write} };
+        for (@tool) {
+            if ( dep($_) ) {
+                system("echo '$in' | $_");
+                return $in;
+            }
+            else {
+                if ( $tool[$#tool] eq $_ ){    # if not even last one of tools is installed
+                    for ( @{ $tool{$os}->{write} } ) { s/^(.+?) .+/$1/ }
+                    print "For magnet link to be placed into clipboard install " . join( ' or ', @{ $tool{$os}->{write} } ) . ". ";
+                    print "It can be found in com.ericasadun.utilities packege." if $os eq 'ios';
+                }
+            }
         }
-    } else {
-        for my $tool( @{$tool{$os}->{read}} ){
-            if( dep($tool) ){ 
+    }
+    else {
+        my @tool = @{ $tool{$os}->{read} };
+        for my $tool (@tool) {
+            if ( dep($tool) ) {
                 my $content = '';
-                open(my $ph, '-|', $tool );
-                while(<$ph>){
+                open( my $ph, '-|', $tool );
+                while (<$ph>) {
                     chomp;
                     $content = $content . ' ' . $_;
                 }
@@ -106,10 +119,16 @@ sub clipboard{
 
                 return $content;
             }
+            else {
+                if ( $tool[$#tool] eq $tool ) {
+                    for ( @{ $tool{$os}->{read} } ) { s/^(.+?) .+/$1/ }
+                    print "To use clipboard content as search keywords install " . join( ' or ', @{ $tool{$os}->{read} } ) . ". ";
+                    print "It can be found in com.ericasadun.utilities packege." if $os eq 'ios';
+                }
+            }
         }
     }
 }
 
-say clipboard();
 
 1;
