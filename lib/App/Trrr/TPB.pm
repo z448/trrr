28b10cf316b3aa3d220064e0dc0194b80dc3379c
z448 <zed448@icloud.com>
2023-11-11 09:54:12 +0100

    tpb(): return 0 if no results

--- lib/App/Trrr/TPB.pm
+++ lib/App/Trrr/TPB.pm
@@ -118,6 +118,7 @@ sub results {
             for (@$content) {
                 $t{title} = $_->{name};
                 #$t{title} =~ s/%([0-9A-Fa-f]{2})/chr(hex($1))/eg;
+                return 0 if $t{title} =~ /No results returned/;
                 $t{api}   = 'tpb';
                 $t{source} = 'piratebay';
 
